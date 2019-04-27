a84eca2553ff63e2b1dadf7c223defef33b0c860
Eric Yang <eric@junto.love>
2019-04-27 18:32:10 -0400

    Add video background and gradient overlay for home header

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,8 +1,19 @@
 
 <template>
-    <div>
-    <cogov-nav></cogov-nav>
-    <div>home component</div>
+    <div class="home">
+        <cogov-nav></cogov-nav>
+        <div class="home__backdrop--cover"></div>                
+
+        <div class="home__backdrop">
+                <video class="home__backdrop--video" playsinline autoplay muted loop>
+                    <source src="./../../assets/videos/sacred-geo.mp4" type="video/mp4">
+                </video>            
+        </div>
+        <!-- <div class="home__cover">
+            <video class="home__video" playsinline autoplay muted loop>
+                <source src="./../../assets/videos/sacred-geo.mp4" type="video/mp4">
+            </video>
+        </div> -->
 
     </div>
 </template>
