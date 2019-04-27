2a9dd4ca319d8449a903c385cd9fb9d53837f26a
Eric Yang <eric@junto.love>
2019-04-27 17:13:39 -0400

    Create cogov navbar and add logo

--- src/pages/Home/Home.vue
+++ src/pages/Home/Home.vue
@@ -1,4 +1,18 @@
 
 <template>
-    <div>hi this is the home component</div>
+    <div>
+    <cogov-nav></cogov-nav>
+    <div>home component</div>
+
+    </div>
 </template>
+
+<script>
+    import cogovNav from '../../components/Nav/Nav.vue'
+
+    export default {
+        components: {
+            cogovNav
+        }
+    }
+</script>
