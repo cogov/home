bbf05792c0d5da19adefec134f251c8b156b2575
Eric Yang <eric@junto.love>
2019-05-05 15:47:06 -0400

    Add items about section

--- src/components/Home/HomeAbout/HomeAbout.vue
+++ src/components/Home/HomeAbout/HomeAbout.vue
@@ -7,10 +7,41 @@
             conflict resolution, and value exchange.
         </h3>    
 
-        <div class="homeAbout__item">
-            <img src="" alt="" class="homeAbout__item--icon">
-            <h2 class="homeAbout__item--title"></h2>
-            <p class="homeAbout__item--description"></p>
+        <div class="homeAbout__items">
+            <div class="homeAbout__item">
+                <img src="" alt="" class="homeAbout__item--icon">
+                <h2 class="homeAbout__item--title">Scalability</h2>
+                <p class="homeAbout__item--description">
+                    New forms of community and business are arising. but unless they can 
+                    create economies of scale, they can’t compete with the old systems and 
+                    attract people out of the old systems. CoGov enables that scalability 
+                    out of the box.                    
+                </p>
+            </div>
+
+            <div class="homeAbout__item">
+                <img src="" alt="" class="homeAbout__item--icon">
+                <h2 class="homeAbout__item--title">Rapid Iteration</h2>
+                <p class="homeAbout__item--description">
+                    We enable people to not just participate in 
+                    systems, but participate in the DESIGN of those systems, 
+                    and redesign them and tweak the design of those systems over and over 
+                    until they get it right.                    
+                </p>
+            </div>
+
+            <div class="homeAbout__item">
+                <img src="" alt="" class="homeAbout__item--icon">
+                <h2 class="homeAbout__item--title">Interoperability</h2>
+                <p class="homeAbout__item--description">
+                    A whole new era of software interoperability is coming forth 
+                    thanks to Holochain. There is no longer a need for clunky and 
+                    burdensome APIs to allow applications to connect with each other. 
+                    CoGov ensures that the same level of interoperability exists between 
+                    Collectives and their Currencies.                    
+                </p>
+            </div>   
         </div>
+                     
     </div>
 </template>
