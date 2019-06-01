752f8dde8586c8d9ebf6ee6966ff201657de3ca1
Eric Yang <eric@junto.love>
2019-06-01 17:55:32 -0400

    Add socials to footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -10,7 +10,32 @@
             <p class="footer__copyright">Copyright 2019 CoGov, All Rights Reserved.</p>
         </div>
         <div class="footer__right">
-
+            <div class="footer__socials">
+                <div class="footer__social">
+                    <img class="footer__social--icon" src="../../assets/images/cogov__messengericon.png" alt="">
+                    <p class="footer__social--text">Messenger</p>
+                </div>
+                <div class="footer__social">
+                    <img class="footer__social--icon" src="../../assets/images/cogov__facebook.png" alt="">
+                    <p class="footer__social--text">Facebook</p>
+                </div>
+                <div class="footer__social">
+                    <img class="footer__social--icon" src="../../assets/images/cogov__twitter.png" alt="">
+                    <p class="footer__social--text">Twitter</p>
+                </div>      
+                <div class="footer__social">
+                    <img class="footer__social--icon" src="../../assets/images/cogov__linkedin.png" alt="">
+                    <p class="footer__social--text">Linkedin</p>
+                </div>
+                <div class="footer__social">
+                    <img class="footer__social--icon" src="../../assets/images/cogov__medium.png" alt="">
+                    <p class="footer__social--text">Medium</p>
+                </div>
+                <div class="footer__social">
+                    <img class="footer__social--icon" src="../../assets/images/cogov__github.png" alt="">
+                    <p class="footer__social--text">Github</p>
+                </div>                                                 
+            </div>
         </div>
 
     </section>
