da3c8403d00f363579b2bf81259c2426a598d047
Eric Yang <eric@junto.love>
2019-06-01 19:58:44 -0400

    Create About page template and refine nav bar

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,9 @@
 
 import Home from './pages/Home/Home.vue'
+import About from './pages/About/About.vue'
 
 export const routes = [
     { path: '/', component: Home },
+    { path: '/about', component: About },
+
 ]
