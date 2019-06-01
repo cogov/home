9b4f3a496400e477e222c78f8bc9c5711296ad52
Eric Yang <eric@junto.love>
2019-06-01 17:41:07 -0400

    Create cogov footer and build logo and copyright

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,7 +1,17 @@
 
 <template>
     <section class="footer">
-        &nbsp;
+        <div class="footer__left">
+            <div class="footer__logo">
+                <img class="footer__logo--image" src="../../assets/images/cogov__logo--white.png" alt="">
+                <p class="footer__logo--text">CoGov</p>
+            </div>
+
+            <p class="footer__copyright">Copyright 2019 CoGov, All Rights Reserved.</p>
+        </div>
+        <div class="footer__right">
+
+        </div>
 
     </section>
 </template>
