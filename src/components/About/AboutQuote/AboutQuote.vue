7a4a2753f993705dfaf865dd80521a56f516c2fb
Eric Yang <eric@junto.love>
2019-06-01 20:08:27 -0400

    Build about quote section

---
+++ src/components/About/AboutQuote/AboutQuote.vue
@@ -0,0 +1,13 @@
+
+<template>
+    <div class="aboutQuote">
+        <p class="aboutQuote__text">"I started CoGov to build systems that are in high
+            resonance with natural patterns and allow humans to recognize the possibility
+            of a new way of life on the planet."
+        </p>
+        <div class="aboutQuote__founder">
+            <img src="" alt="" class="aboutQuote__founder--avatar">
+            <p class="aboutQuote__founder--name">Raymond Powell</p>
+        </div>
+    </div>
+</template>
--- src/pages/About/About.vue
+++ src/pages/About/About.vue
@@ -4,6 +4,9 @@
         <!-- navigation -->
         <cogov-nav></cogov-nav>       
 
+        <!-- quote -->
+        <cogov-about-quote></cogov-about-quote>
+
         <!-- contact -->
         <cogov-whitepaper></cogov-whitepaper>
 
@@ -14,13 +17,14 @@
 
 <script>
     import cogovNav from '../../components/Nav/Nav.vue'
+    import cogovAboutQuote from '../../components/About/AboutQuote/AboutQuote.vue'
     import cogovWhitepaper from '../../components/Whitepaper/Whitepaper.vue'
     import cogovFooter from '../../components/Footer/Footer.vue'
 
     export default {
         components: {
             cogovNav,
-
+            cogovAboutQuote,
             cogovWhitepaper,
             cogovFooter
         }
