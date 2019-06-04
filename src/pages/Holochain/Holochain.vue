70d7dbdaa470c19cddd8f246f95c20617762e290
Eric Yang <eric@junto.love>
2019-06-03 22:32:33 -0400

    Build Holochain page template

---
+++ src/pages/Holochain/Holochain.vue
@@ -0,0 +1,27 @@
+
+<template>
+    <div class="holochain">
+        <!-- navigation -->
+        <cogov-nav></cogov-nav>       
+
+        <!-- contact -->
+        <cogov-whitepaper></cogov-whitepaper>
+
+        <!-- footer -->
+        <cogov-footer></cogov-footer>         
+    </div>
+</template>
+
+<script>
+    import cogovNav from '../../components/Nav/Nav.vue'
+    import cogovWhitepaper from '../../components/Whitepaper/Whitepaper.vue'
+    import cogovFooter from '../../components/Footer/Footer.vue'
+
+    export default {
+        components: {
+            cogovNav,
+            cogovWhitepaper,
+            cogovFooter
+        }
+    }
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 
 import Home from './pages/Home/Home.vue'
 import About from './pages/About/About.vue'
+import Holochain from './pages/Holochain/Holochain.vue'
 
 export const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
+    { path: '/holochain', component: Holochain },
 
 ]
