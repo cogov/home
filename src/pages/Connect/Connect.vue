adf8b80f799dbcb71cdfbeec3308e068082eeef8
Eric Yang <eric@junto.love>
2019-06-11 10:45:34 -0400

    Create Connect page

---
+++ src/pages/Connect/Connect.vue
@@ -0,0 +1,28 @@
+
+<template>
+    <div class="connect">
+        <!-- navigation -->
+        <cogov-nav></cogov-nav>       
+
+
+        <!-- contact -->
+        <cogov-whitepaper></cogov-whitepaper>
+
+        <!-- footer -->
+        <cogov-footer></cogov-footer>         
+    </div>
+</template>
+
+<script>
+    import cogovNav from '../../components/Nav/Nav.vue'
+    import cogovWhitepaper from '../../components/Whitepaper/Whitepaper.vue'
+    import cogovFooter from '../../components/Footer/Footer.vue'
+
+    export default {
+        components: {
+            cogovNav,
+            cogovWhitepaper,
+            cogovFooter
+        }
+    }
+</script>
--- src/routes.js
+++ src/routes.js
@@ -3,11 +3,13 @@ import Home from './pages/Home/Home.vue'
 import About from './pages/About/About.vue'
 import Holochain from './pages/Holochain/Holochain.vue'
 import Whitepaper from './pages/Whitepaper/Whitepaper.vue'
+import Connect from './pages/Connect/Connect.vue'
 
 export const routes = [
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/holochain', component: Holochain },
     { path: '/whitepaper', component: Whitepaper },
+    { path: '/connect', component: Connect },
 
 ]
