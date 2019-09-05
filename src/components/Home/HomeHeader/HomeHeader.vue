684fb0052e3b22c05dd6f158dbba18cb655649e8
Raymond D. Powell <ray@newearth.email>
2019-09-04 20:47:38 -0600

    wip

--- src/components/Home/HomeHeader/HomeHeader.vue
+++ src/components/Home/HomeHeader/HomeHeader.vue
@@ -15,7 +15,7 @@
             
             <h3 class="homeHeader__subtitle">
                 CoGov fosters the emergence of coherent collective intelligence by enabling the rapid iteration 
-                of governance and social coordination Systems that YOU design!
+                of governance and social coordination systems that YOU design!
             </h3>            
 
         </div>
