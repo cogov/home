8344ec8073fe96097dbb5ddaf43a3993a42de338
Eric Yang <eric@junto.love>
2019-09-18 16:08:30 +0100

    Add photos to protocol

--- src/components/Protocol/ProtocolContent/ProtocolContent.vue
+++ src/components/Protocol/ProtocolContent/ProtocolContent.vue
@@ -6,7 +6,7 @@
       <br />
       <br />With Holochain, every application is made up of one or more protocols. Each application runs its own holochain on its own terms. Each device that is running an application has a private source <a href="https://medium.com/@rayzer42/9039eba653a1" class="addlink" target="blank">chain</a>  that holds the app's “DNA”—the code that runs the app and ensures the protocols are being adhered to. Each app stores its data using the same type of <a href="https://en.wikipedia.org/wiki/BitTorrent" target="blank" class="addlink">distributed storage standards</a> that have been in operation for decades. This creates redundancy and validation of application data across randomized nodes.
     </p>
-    <!-- <img src="" alt="" class="protocolContent__image"> -->
+    <img src="../../../assets/images/cogov__protocol--one.png" alt="" class="protocolContent__image protocolContent__image--one">
     <br />
     <br />
     <p class="protocolContent__body">
@@ -14,7 +14,7 @@
       <br />
       <br />The CoGov Collective Base Entries “hang off” the DNA entry of any holochain using <a href='https://developer.holochain.org/api/latest/hdk/api/fn.link_entries.html' class="addlink" target="blank">Holochain links</a>. Other CoGov entries then hang off of that Collective Base Entry, and each other. This visual shows a sample of various types of entries and how they “hang off” each other. It is by no means a complete diagram.
     </p>
-    <!-- <img src="" alt="" class="protocolContent__image"> -->
+    <img src="../../../assets/images/cogov__protocol--two.png" alt="" class="protocolContent__image protocolContent__image--two">
     <p class="protocolContent__body">
       However, the above is not an illustration of how data is actually stored. For more information on that, see this article: <a href="https://medium.com/@rayzer42/9039eba653a1" class="addlink" target="blank"> Monotonic Hash Chains? Distributed Ledger Technology? Holochain!</a>
       <br />
@@ -62,6 +62,9 @@
       <br />
       <br />A very early 2016 mock-up of what a CoGov Ledger Browser might look like can be found at
       <a href='http://demo.cogov.tech/' class="addlink" target="blank">http://demo.cogov.tech/</a>
+<br><br>
+    <img src="../../../assets/images/cogov__protocol--three.png" alt="" class="protocolContent__image protocolContent__image--three">
+
     </p>
   </div>
 </template>
