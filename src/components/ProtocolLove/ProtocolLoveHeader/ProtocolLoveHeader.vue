8ada7fda9a1f3c46a8ccf86f608ef964c0675831
Eric Yang <eric@junto.love>
2020-02-06 20:28:46 +0100

    Update protocol love and war on sense making

--- src/components/ProtocolLove/ProtocolLoveHeader/ProtocolLoveHeader.vue
+++ src/components/ProtocolLove/ProtocolLoveHeader/ProtocolLoveHeader.vue
@@ -4,7 +4,7 @@
             <p class="protocolLoveHeader__title">Protocol.Love</p>
             <p class="protocolLoveHeader__subtitle">
                 Protocol.Love fosters the emegence of a socioeconomic system imbued with Loving Kindness
-                for organizations who choose a commitment to <a href="*ARTICLE_URL*" target="_blank">Vulnerable Transparency</a>.
+                for organizations who choose a commitment to <a href="*ARTICLE_URL*" target="_blank" class="removelink" style="text-decoration: underline">Vulnerable Transparency</a>.
             </p>  
     </div>
 </template>
